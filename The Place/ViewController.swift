ca5dc0b17679846fcf03df8ff1deb2325e03ca2e
Arthur D <clarkk.inc@gmail.com>
2020-01-27 18:52:24 +0300

    Initial Commit

---
+++ The Place/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Place
+//
+//  Created by Arthur Daniyarov on 27.01.2020.
+//  Copyright © 2020 Arthur Daniyarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
